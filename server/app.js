e297adc6aa6abc924f7b797dc6433a69da30f251
Delta Student <rajneeshavneeshkar@gmail.com>
2025-11-24 18:33:33 +0530

    config: Update API URLs for Render deployment - backend at https://get-hired-easy.onrender.com

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 };
